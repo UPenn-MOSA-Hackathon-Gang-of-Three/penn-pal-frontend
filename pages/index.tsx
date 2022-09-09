8355fb8c6af0c3e9fdc2e83034e0363727d2095f
Dan Fitz <dan@danfitz.com>
2022-09-09 10:06:26 -0400

    Added lottie animation and improved styling of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { Container, Flex, Box, Heading, Text } from '@chakra-ui/react';
+import Lottie from 'lottie-react';
+import collaborateAnimation from 'assets/collaborateAnimation.json';
 
 import Button from 'components/button';
 
@@ -8,24 +10,36 @@ const Home: NextPage = () => {
   return (
     <Container
       maxW={{ base: '100%', lg: '85%' }}
-      sx={{ minHeight: '100vh', display: 'flex', alignItems: 'center' }}
+      display='flex'
+      alignItems='center'
+      minHeight='100vh'
     >
-      <Flex flexDirection={{ base: 'column-reverse', lg: 'row' }}>
-        <Box w={{ base: '100%', lg: '60%' }}>
+      <Flex
+        flexDirection={{ base: 'column-reverse', lg: 'row' }}
+        justifyContent='space-between'
+        alignItems='center'
+      >
+        <Box
+          w={{ base: '100%', lg: '40%' }}
+          textAlign={{ base: 'center', lg: 'left' }}
+          mt={{ base: 7, lg: 0 }}
+        >
           <Heading as='h1' size='2xl' mb={4}>
-            Mentor-mentee matching
+            Match mentees with mentors
           </Heading>
           <Text mb={7}>
             Help professionals and students connect with others around the world
           </Text>
-          <Button size='sm' sx={{ mr: 3 }}>
+          <Button size='sm' sx={{ mr: 3, mt: 3 }}>
             Create an event
           </Button>
-          <Button size='sm' outline>
+          <Button size='sm' outline sx={{ mt: 3 }}>
             View status
           </Button>
         </Box>
-        <Box>Lottie goes here</Box>
+        <Box w={{ base: '100%', lg: 'calc(60% - 1rem)' }}>
+          <Lottie animationData={collaborateAnimation} loop />
+        </Box>
       </Flex>
     </Container>
   );
